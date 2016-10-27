b6d013c6283089e628097d44375bfbab54ed9524
rogervieira <Ahyra0003>
2016-10-27 18:15:01 -0200

    correção bug de edição dos cães (não estava notificando quando faltava foto)

--- AdoteNaoCompreSITE/admin.py
+++ AdoteNaoCompreSITE/admin.py
@@ -3,9 +3,11 @@ from AdoteNaoCompreSITE.models.dog import Dog
 from AdoteNaoCompreSITE.models.post import Post
 from AdoteNaoCompreSITE.models.breed import Breed
 from AdoteNaoCompreSITE.models.state import State
+from AdoteNaoCompreSITE.models.advertisement import Advertisement
 
 admin.site.register(Dog)
 admin.site.register(Post)
 admin.site.register(Breed)
 admin.site.register(State)
+admin.site.register(Advertisement)
 
--- AdoteNaoCompreSITE/models/advertisement.py
+++ AdoteNaoCompreSITE/models/advertisement.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Advertisement(models.Model):
-    """description of class"""
     Id = models.AutoField(primary_key=True)
     Descricao = models.CharField(max_length=100)
-    Banner = Foto = models.FileField(upload_to='Images/%Y/%m/%d', blank=False)
+    Banner = models.FileField(upload_to='Images/%Y/%m/%d', blank=False)
     Dono = models.ForeignKey(User)
-    DataRegistro = models.IntegerField(blank=True, null=True)
-    Email = models.CharField(blank=True, null=True)
-    Telefone = models.DateTimeField(null=True)
+    DataRegistro = models.DateTimeField(null=True)
+    Email = models.CharField(blank=True, null=True, max_length=100)
+    Telefone = models.IntegerField(blank=True, null=True)
 
 
